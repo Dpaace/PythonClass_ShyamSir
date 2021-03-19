4baaa7dfd3d788e83c45c8e318088c68f40608a8
DPAACE <dipeshnepali767@gmail.com>
2021-03-19 14:18:37 +0545

    while loop and nested loop

---
+++ nested loop.py
@@ -0,0 +1,9 @@
+'''
+Nested loop
+
+for i in range(4):          outer loop---row
+    for i in range(3):      inner loop---column
+        <statements>
+
+
+'''
--- while loop.py
+++ while loop.py
@@ -1,16 +1,42 @@
 '''
-    factorial program
-num = int(input("Enter any number:"))
-product = 1
-for i in range(num, 0, -1):
-    product = product * i
-print(f"The factorial of the given number {num} is {product}")
-
+        While loop
+syntax:
+initialization
+while <expression>:
+    <statement>
 '''
 
-num = int(input("Enter any number:"))
-for i in range(1,11):
-    product = num * i
-    print(f"{num} x {i} = {product} ")
+'''
+i = 1
+while i <= 10:
+    print(i)        # i -=1 i.e. i = i -1
+    i += 1          # i = i + 1
+'''
+#
+'''
+import random
+number = random.randint(1, 10)
+num_guess = int(input("Enter the guessing number:"))
+while number != num_guess:
+    num_guess = int(input("OOPs! Try again"))
+else:
+    print("Congralutations you are right")
+'''
+
+import random
+number = random.randint(1, 10)
+while true:
+    num_guess = int(input("Enter the guessing number:"))
+    if num_guess == number:
+        print("Congralutations you are right")
+        break
+    else:
+        print("Try again")
+
+
+
+
+
+
 
 
